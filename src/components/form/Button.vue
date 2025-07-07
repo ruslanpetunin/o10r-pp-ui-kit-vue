60885bd7d6ff3d8950fd7ffd2d40a308cb07b50f
Ruslan <r.petunin@it.ecommpay.com>
2025-07-07 15:51:41 +0300

    Fix

--- src/components/form/Button.vue
+++ src/components/form/Button.vue
@@ -1,3 +1,13 @@
 <template>
   <button class="ui-button"><slot /></button>
 </template>
+
+<style scoped>
+.ui-button {
+  padding: 0.5em 1em;
+  background: #007bff;
+  color: white;
+  border-radius: 4px;
+  border: none;
+}
+</style>
