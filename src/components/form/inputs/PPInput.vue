2f1bd677078f60506f07c5b7dc0bff7b18a0af8a
Ruslan <r.petunin@it.ecommpay.com>
2025-07-08 21:21:52 +0300

    PPInput

---
+++ src/components/form/inputs/PPInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <input :type="type">
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  type: 'text' | 'number' | 'email' | 'password' | 'tel',
+}>();
+</script>
+
+<style scoped>
+input {
+  display: inline-block;
+  width: 100%;
+}
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as PPButton } from './form/PPButton.vue';
+export { default as PPInput } from './form/inputs/PPInput.vue';
 export { default as PPAccordion } from './list/Accordion/PPAccordion.vue';
 export { default as PPAccordionItem } from './list/Accordion/PPAccordionItem.vue';
