56f01a7a615913adb837e3e47fa04cdc98e2c30e
Ruslan <r.petunin@it.ecommpay.com>
2025-08-06 16:44:27 +0300

    Change plugin connection

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import type { App } from 'vue';
+import type { App, FunctionPlugin } from 'vue';
 import * as components from './components';
 import './style.css';
 
 export * from './components';
 
-export default {
-    install(app: App) {
-        for (const key in components) {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            app.component(key, (components as any)[key]);
-        }
-    }
-};
+const connect: FunctionPlugin = (app: App) => {
+  for (const key in components) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    app.component(key, (components as any)[key]);
+  }
+}
+
+export default connect;
